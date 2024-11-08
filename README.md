8c9e29a16ece7003c895b5ca48629339e5f335d8
Bhavya Chavda <bhavyachavda55@gmail.com>
2024-11-08 08:03:42 +0530

    build logout feature

--- README.md
+++ README.md
@@ -11,3 +11,15 @@
 - Create a Outlet
 - Create a Footer
 - Create a Login Page
+- Install axios
+- CORS - install cors in backend => add middleware to with configuration: origin, credentials: true
+- Whenever you're making API call so pass axios => { withCredentials: true}
+- Install react-redux + @reduxjs/toolkit 
+- configureStore => Provider => createSlice => add reducer to store 
+- Add redux devtools in chrome
+- Login and see if your data is coming properly in the store
+- NavBar should update as soon as user logs in
+- Refactor our code to add constants file + create a components folder
+- You should not be access other routes without login
+- If token is not present, redirect user to login page 
+- Logout feature
---
+++ src/components/Login.jsx
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("rahul@gmail.com");
+  const [password, setPassword] = useState("Rahul@123");
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res?.data));
+      return navigate("/");
+    } catch (error) {
+      setError(error?.response?.data || "something went wrong")
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center m-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-4">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-4">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <p className="text-red-500">{error}</p>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
